ef505a3b29be2fd5f1d0373f8db305f8278a74c8
mahnazsaiyed <mahnazsaiyed@gmail.com>
2023-07-18 10:05:40 -0700

    Initial Commit

---
+++ final project to-do list/final_project_to_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_project_to_do_listApp.swift
+//  final project to-do list
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct final_project_to_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
